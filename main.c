bf5c42f9fe717d16d6257ed77c204da0b626474a
jpbusta10 <103268894+jpbusta10@users.noreply.github.com>
2022-06-21 10:34:41 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct
+{
+char nombre_ingrediente[20];
+float cantidad; //en kg o en litro segun el tipo de ingrediente
+char tipo[20]; /// "liquido" "solido"
+float costo; ///costo por kg o por litro según corresponda
+}StockIngrediente;
 
 int main()
 {
    
     return 0;
+}
+
+void despersistenciaStock(StockIngrediente lista [],int*validos)
+{
+    FILE* fp;
+    int i=0;
+    fp=fopen("stockingredientes.bin","rb");
+
+    if(fp!=NULL)
+    {
+        while(fread(&lista[i],sizeof(StockIngrediente),1,fp)>0)
+        {
+            i++;
+        }
+        (*validos)=i;
+    }
+
+}
+void mostrarStock(StockIngrediente p)
+{
+    printf("[");
+    printf("NOMBRE: %s \n",p.nombre_ingrediente);
+    printf("CANRIDAD: %f \n",p.cantidad);
+    printf("TIPO: %s \n",p.tipo);
+    printf("COSTO: %f \n",p.costo);
+    printf("]\n");
+}
+void muestraListaStock(StockIngrediente lista[],int validos)
+{
+    for(int i=0;i<validos;i++)
+    {
+        mostrarStock(lista[i]);
+    }
 }
